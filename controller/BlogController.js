cb5899cf7287aec829709ae6fa15bca291f6b786
TerranceRivers <109315955+TerranceRivers@users.noreply.github.com>
2023-06-05 11:59:33 -0400

    create-get-by-id-update-by-id

--- controller/BlogController.js
+++ controller/BlogController.js
@@ -1,3 +1,4 @@
+const BlogPost = require("../model/Blog");
 const Blog = require("../model/Blog");
 
 const getAllBlogs = async (req, res) => {
@@ -9,17 +10,54 @@ const getAllBlogs = async (req, res) => {
   }
 };
 
-const createTodo = async (req, res) => {
-    try {
-      const newTodo = await new Todo(req.body);
-      const saveTodo = await newTodo.save();
-      res.status(200).json({ success: true, data: saveTodo });
-    } catch (error) {
-      res.status(500).json({ success: false, message: error.message });
-    }
-  };
+const createBlogPost = async (req, res) => {
+  try {
+    const newPost = await new BlogPost(req.body);
+    const savePost = await newPost.save();
+    res.status(200).json({ success: true, data: savePost });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
+const getBlogById = async (req, res) => {
+  try {
+    const { id } = req.query;
+    const blogPost = await BlogPost.findById(id);
+    if (!blogPost) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Blog post not found" });
+    }
+    res.status(200).json({ success: true, data: blogPost });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+const updateBlogPost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, content, author } = req.body;
+    const updatedBlogPost = await BlogPost.findByIdAndUpdate(
+      id,
+      { title, content, author },
+      { new: true }
+    );
+    if (!updatedBlogPost) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Blog post not found" });
+    }
+    res.status(200).json({ success: true, data: updatedBlogPost });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 module.exports = {
-    getAllBlogs,
-}
+  getAllBlogs,
+  createBlogPost,
+  getBlogById,
+  updateBlogPost,
+};
--- routes/blog.js
+++ routes/blog.js
@@ -1,7 +1,15 @@
 var express = require("express");
 var router = express.Router();
-const { getAllBlogs } = require("../controller/BlogController");
+const {
+  getAllBlogs,
+  createBlogPost,
+  getBlogById,
+  updateBlogPost,
+} = require("../controller/BlogController");
 
 router.get("/all-blogs", getAllBlogs);
+router.post("/create", createBlogPost);
+router.get("/id", getBlogById);
+router.put("/update/:id", updateBlogPost)
 
 module.exports = router;
